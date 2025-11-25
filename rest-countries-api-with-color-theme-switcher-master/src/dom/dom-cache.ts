ccfbb20c012320841dd4ce8412e775c8b9710681
Amanda Ogletree <amandaogletree19@gmail.com>
2025-11-25 09:49:27 -0500

    Hid card template in HTML file. Added input and select styling for user input feedback and accessibility. Created new TS files for DOM variables and for Card class. Added new folders for organization.

---
+++ rest-countries-api-with-color-theme-switcher-master/src/dom/dom-cache.ts
@@ -0,0 +1,8 @@
+/*
+    Export:
+    Cached Dom Variables for Dynamic Content Generation of
+    Country Card List Items
+*/
+
+export const cardsBatch = document.getElementById('cards-batch')
+export const cardTemplate = document.querySelector('.card-template')
---
+++ rest-countries-api-with-color-theme-switcher-master/src/models/Card.ts
@@ -0,0 +1,37 @@
+class Card{
+    flag: string
+    name: string
+    population: number
+    region: string
+    capital: string
+
+    constructor(flag:string, name:string, population:number, region:string, capital:string){
+        this.flag = flag,
+        this.name =name,
+        this.population = population,
+        this.region = region,
+        this.capital = capital
+    }
+}
+
+/*
+    REST Countries API in regards to the requirements of the
+        FrontendMentor Challenge
+
+    Provides for each country:
+    - A PNG & SVG along with alt text.
+        || field: 'flags' [flag is not the same]
+        || type: string
+    - A Common, official, and native name
+        || field: 'name'
+        || type: string
+    - A Population count
+        || field: 'population'
+        || type: number
+    - A Region
+        || field: 'region'
+        || type: string
+    - A Capital
+        || field: 'capital'
+        || type: string
+*/
